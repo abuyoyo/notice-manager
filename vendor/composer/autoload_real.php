4810fe940042f00e6a5683bd34789e1931eb3bb7
abuyoyo <abuyoyo@users.noreply.github.com>
2022-08-06 16:47:50 +0300

    vendor - Remove 3rd party dependency + composer.lock
    - remove yahnis-elsts/plugin-update-checker

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit99e41faf4c8cd1b54927a50ca0e7ee5c
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit99e41faf4c8cd1b54927a50ca0e7ee5c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit99e41faf4c8cd1b54927a50ca0e7ee5c', 'loadClassLoader'));
